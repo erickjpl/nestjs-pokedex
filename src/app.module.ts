cf398c87e6ce6c789df15d27f494e7a671744b7f
Erick Perez <erickjpl@gmail.com>
2023-10-02 23:31:37 -0400

    agregando mongo y el schema de mongo del pokemon

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 
 @Module({
-  imports: [PokemonModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    PokemonModule,
+  ],
   controllers: [],
   providers: [],
 })
